9a17ce438311d0590f1ef1da378b30e2ad22b35d
wssgcg1213 <Liuchenling@B1ackRainFlake.local>
2013-11-18 21:59:41 +0800

    Up

---
+++ getElementsByClassName.js
@@ -0,0 +1,28 @@
+var getElementsByClassName = function(c){
+	//var timestamp0 = new Date().getTime();
+	var eles = new Array();
+	var tags = document.getElementsByTagName("*");
+	for(var i=0;i<tags.length;i++){
+		var temp = tags[i].className.split(" ");
+		for(j in temp)
+ 			if(temp[j]===c)eles[eles.length]=tags[i];      //eles.push(tags[i]);
+	}
+	//var timestamp1=new Date().getTime();
+	//console.log(timestamp1-timestamp0+"ms");
+	return eles;
+}
+
+
+var getElementsByClassName2 = function(c){
+	//var timestamp0 = new Date().getTime();
+	var tags = document.getElementsByTagName("*");
+	var result = new Array();
+	var pattern = new RegExp("\\b"+c+"\\b","i");    		//pattern = new RegExp("\\b"+c+"(?!\\w)","i");
+		for(var i=0; i<x.length; i++)
+			if(pattern.test(tags[i].className))result.push(tags[i]);
+	//var timestamp1 = new Date().getTime();
+	//console.log(timestamp1-timestamp0+"ms");
+		return result;
+	}
+
+//测试结果:后一个函数不论使用哪个正则执行效率是前一个3倍以上; 
---
+++ readme.md
@@ -0,0 +1,10 @@
+@@User: B1ackRainFlake @@
+=================
+
+	JavaScript document.getElementsByClassName()方法的模拟;
+
+----------------
+
+###	    因为IE5.5不支持getElementsByTagName,所以只支持IE6+;
+###     在IETester中发现IE8表现不正常(无视它);
+
